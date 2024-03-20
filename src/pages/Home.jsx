8a2a9702d6d5ed14848e41e252b493f1866d935c
Levi Ekström <vojtechlevi1@hotmail.com>
2024-03-20 14:15:17 +0100

    css fix

--- src/pages/Home.jsx
+++ src/pages/Home.jsx
@@ -1,7 +1,26 @@
 import React from "react";
 
 const Home = () => {
-  return <div>Home</div>;
+  return (
+    <>
+      <main className="w-full h-screen flex flex-col justify-center items-center">
+        <div className="">
+          <h1>Event 24 april</h1>
+          <p>
+            Välkomna <span>-</span> mingel <span>-</span> Webbutvecklare och
+            Digital Designers på YRGO <span>-</span> det blir kul!
+          </p>
+        </div>
+        <div>
+          <p className="">Vem är du?</p>
+          <div>
+            <button>Student</button>
+            <button>Företag</button>
+          </div>
+        </div>
+      </main>
+    </>
+  );
 };
 
 export default Home;
