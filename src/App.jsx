091c68b0ea518fc220c915f5ecdc3d42af73c9d6
Levi Ekström <vojtechlevi1@hotmail.com>
2024-03-20 13:32:51 +0100

    added routes & pages

--- src/App.jsx
+++ src/App.jsx
@@ -1,87 +1,21 @@
-import { useEffect, useState } from "react";
-import { supabase } from "./client";
+import { Route, Routes } from "react-router-dom";
+import Home from "./pages/Home";
+import Student from "./pages/Student";
+import Company from "./pages/Company";
+import Login from "./pages/Login";
+import Signup from "./pages/Signup";
 
 function App() {
-  const [countries, setCountries] = useState([]);
-
-  useEffect(() => {
-    getCountries();
-  }, []);
-
-  async function getCountries() {
-    const { data } = await supabase.from("countries").select();
-    setCountries(data);
-  }
-
-  async function addCountry() {
-    const { error } = await supabase
-      .from("countries")
-      .insert({ name: "Denmark" });
-  }
-
-  const [formData, setFormData] = useState({
-    fullName: "",
-    email: "",
-    password: "",
-  });
-
-  function handleChange(event) {
-    setFormData((prevFormData) => {
-      return {
-        ...prevFormData,
-        [event.target.name]: event.target.value,
-      };
-    });
-  }
-
-  async function handleSubmit(e) {
-    e.preventDefault();
-    try {
-      const { data, error } = await supabase.auth.signUp({
-        email: formData.email,
-        password: formData.password,
-        options: {
-          data: {
-            full_name: formData.fullName,
-          },
-        },
-      });
-    } catch (error) {
-      alert(error);
-    }
-  }
-
   return (
-    <div>
-      {/* <form onSubmit={handleSubmit}>
-        <input
-          type="text"
-          placeholder="Fullname"
-          name="fullName"
-          onChange={handleChange}
-        />
-        <input
-          type="email"
-          placeholder="Email"
-          name="email"
-          onChange={handleChange}
-        />
-        <input
-          type="password"
-          placeholder="Password"
-          name="password"
-          onChange={handleChange}
-        />
-
-        <button type="submit">Submit</button>
-      </form> */}
-      <ul>
-        {countries.map((country) => (
-          <li key={country.id}>{country.name}</li>
-        ))}
-      </ul>
-      <button onClick={addCountry}>add</button>
-    </div>
+    <>
+      <Routes>
+        <Route path="/" element={<Home />} />
+        <Route path="/student" element={<Student />} />
+        <Route path="/company" element={<Company />} />
+        <Route path="/login" element={<Login />} />
+        <Route path="/signup" element={<Signup />} />
+      </Routes>
+    </>
   );
 }
 
