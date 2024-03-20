3ee858154cb918b9f656bb08e4c1995e326e66f0
Levi Ekström <vojtechlevi1@hotmail.com>
2024-03-20 16:03:12 +0100

    landing page + added route for 404

--- src/pages/Home.jsx
+++ src/pages/Home.jsx
@@ -1,21 +1,30 @@
 import React from "react";
+import { Link } from "react-router-dom";
 
 const Home = () => {
   return (
     <>
-      <main className="w-full h-screen flex flex-col justify-center items-center">
-        <div className="">
-          <h1>Event 24 april</h1>
-          <p>
-            Välkomna <span>-</span> mingel <span>-</span> Webbutvecklare och
-            Digital Designers på YRGO <span>-</span> det blir kul!
+      <main className="w-full h-screen flex flex-col justify-around items-center">
+        <div className="w-full px-8 flex flex-col gap-6">
+          <h1 className="text-8xl">Event 24 april</h1>
+          <p className="text-base">
+            Välkomna - mingel - Webbutvecklare och Digital Designers på YRGO -
+            det blir kul!
           </p>
         </div>
-        <div>
-          <p className="">Vem är du?</p>
-          <div>
-            <button>Student</button>
-            <button>Företag</button>
+        <div className="flex flex-col gap-4 justify-center items-center">
+          <p className="text-xl">Vem är du?</p>
+          <div className="flex gap-4">
+            <Link to="/student">
+              <button className="border border-black rounded-[32px] px-4 py-3">
+                Student
+              </button>
+            </Link>
+            <Link to="/company">
+              <button className="border border-black rounded-[32px] px-4 py-3">
+                Företag
+              </button>
+            </Link>
           </div>
         </div>
       </main>
