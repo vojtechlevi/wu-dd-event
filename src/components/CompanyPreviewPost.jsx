9fe1b28c5c9c27a0f9e1494c0095d5d73f306592
JoarHansson <124060119+JoarHansson@users.noreply.github.com>
2024-03-22 15:47:37 +0100

    add functionality for writing form data to db

--- src/components/CompanyPreviewPost.jsx
+++ src/components/CompanyPreviewPost.jsx
@@ -1,12 +1,44 @@
-import React from "react";
+import { supabase } from "../client";
 
-const CompanyPreviewPost = ({ counter, setCounter }) => {
+const CompanyPreviewPost = ({ counter, setCounter, answer }) => {
+  async function writeToDatabase() {
+    try {
+      const { error } = await supabase.from("companies").insert({
+        name: answer.name,
+        bransch: answer.sector,
+        employees: answer.employees,
+        employees: answer.employees,
+        focusareas: answer.focusareas,
+        companyURL: answer.url,
+        companyType: answer.type,
+        softwares: answer.software,
+        top5: answer.quickQuestions,
+        internType: answer.internType,
+        internCount: answer.internCount,
+        internshipStartDate: "2000-01-01", // for now
+        internshipEndDate: "2050-01-01", // for now
+        internshipSkillReq: answer.skillsRequested,
+        description: answer.description,
+      });
+      if (error) {
+        console.error("Error inserting data:", error);
+      } else {
+        console.log("Data inserted successfully");
+      }
+    } catch (err) {
+      console.error("Unexpected error:", err);
+    }
+  }
+
   return (
     <>
       <h2 className="text-4xl">Din preview</h2>
       <button className="border border-black p-2">Redigera</button>
       <button
-        onClick={() => setCounter(counter + 1)}
+        onClick={() => {
+          setCounter(counter + 1);
+          writeToDatabase();
+        }}
         className="border border-black p-2">
         Klar!
       </button>
