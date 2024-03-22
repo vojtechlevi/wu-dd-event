d4e1cb227d8daf4a94b02f3e320f7336dd700d5d
JoarHansson <124060119+JoarHansson@users.noreply.github.com>
2024-03-22 14:06:23 +0100

    Add all (as of now) names/imports of form pages

--- src/pages/Company.jsx
+++ src/pages/Company.jsx
@@ -10,13 +10,34 @@ import CompanyFocusAreas from "../components/CompanyFocusAreas";
 import CompanyUrl from "../components/CompanyUrl";
 import CompanyType from "../components/CompanyType";
 import CompanySoftware from "../components/CompanySoftware";
+import CompanyQuickQuestions from "../components/CompanyQuickQuestions";
 import CompanyInternType from "../components/CompanyInternType";
 import CompanyInternCount from "../components/CompanyInternCount";
 import CompanyInternshipDuration from "../components/CompanyInternshipDuration";
+import CompanySkillsRequested from "../components/CompanySkillsRequested";
+import CompanyDescription from "../components/CompanyDescription";
+import CompanyPreviewPost from "../components/CompanyPreviewPost";
+import CompanyConfirmation from "../components/CompanyConfirmation";
+import CompanyEventStats from "../components/CompanyEventStats";
 // import more form pages as needed
 
 const Company = () => {
   const [counter, setCounter] = useState(0);
+  const [answer, setAnswer] = useState({
+    name: "",
+    sector: "",
+    employees: 0,
+    focusAreas: {},
+    url: "",
+    type: "",
+    software: {},
+    quickQuestions: {},
+    internType: "",
+    internCount: 0,
+    internshipDuration: "",
+    skillsRequested: {},
+    description: "",
+  });
 
   const formPages = [
     CompanyGreeting,
@@ -27,9 +48,15 @@ const Company = () => {
     CompanyUrl,
     CompanyType,
     CompanySoftware,
+    CompanyQuickQuestions,
     CompanyInternType,
     CompanyInternCount,
     CompanyInternshipDuration,
+    CompanySkillsRequested,
+    CompanyDescription,
+    CompanyPreviewPost,
+    CompanyConfirmation,
+    CompanyEventStats,
     // add more form pages as needed
   ];
 
@@ -44,7 +71,12 @@ const Company = () => {
           <BackArrow />
         </button>
         <div className="h-full flex flex-col justify-center items-center gap-4 p-8">
-          <FormPage counter={counter} setCounter={setCounter} />
+          <FormPage
+            counter={counter}
+            setCounter={setCounter}
+            answer={answer}
+            setAnswer={setAnswer}
+          />
         </div>
       </div>
     </>
