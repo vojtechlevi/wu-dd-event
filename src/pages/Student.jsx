8b7846962fc23fdac2a908813e6e98c23510a2ba
Levi Ekström <vojtechlevi1@hotmail.com>
2024-03-25 13:30:33 +0100

    company list internType fix

--- src/pages/Student.jsx
+++ src/pages/Student.jsx
@@ -12,7 +12,7 @@ const Student = () => {
   const [menu, setMenu] = useState(true);
   const [companies, setCompanies] = useState([]);
   // const name = user && user.user.user_metadata ? user.user.user_metadata.full_name : ""; //email
-  //const name = user.user_metadata.user_name; //github - figma
+  const name = user.user_metadata.user_name; //github - figma
 
   useEffect(() => {
     getCompanies();
@@ -31,6 +31,9 @@ const Student = () => {
   async function signOut() {
     const { error } = await supabase.auth.signOut();
   }
+
+  console.log(user.user_metadata.user_name);
+  console.log(companies.internType);
 
   return (
     <section className="px-4">
@@ -73,10 +76,36 @@ const Student = () => {
         <ul>
           {companies.map((company) => (
             <li key={company.id} className="mt-4">
-              <h3 className="flex justify-between items-center">
-                {company.name} <Heart size={16} />
+              <h3 className="text-lg flex justify-between items-center mb-2">
+                {company.name}
+                <Heart size={16} /* fill="red" stroke="black"  */ />
               </h3>
-              <p>Vi söker:</p>
+              {company && company.internType.length > 0 ? (
+                Array.isArray(company.internType) &&
+                company.internType.length > 0 ? (
+                  <div className="flex items-center gap-2">
+                    <p className=" text-xs">Vi söker:</p>
+                    {company.internType.map((type, index) => (
+                      <p
+                        key={index}
+                        className="border px-2 py-1 rounded-lg text-xs"
+                      >
+                        {type}
+                      </p>
+                    ))}
+                  </div>
+                ) : (
+                  <div className="flex items-center">
+                    <p className="mr-2 text-xs">Vi söker:</p>
+                    <p className="border px-2 py-1 rounded-lg text-xs">
+                      {company.internType}
+                    </p>
+                  </div>
+                )
+              ) : (
+                <p className="text-xs">Söker ej</p>
+              )}
+              <div className="w-full h-[1px] bg-gray-200 mt-4"></div>
             </li>
           ))}
         </ul>
