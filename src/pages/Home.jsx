e7da92405859d32c1d8cbc9f0bc17efc23ff5b6d
Levi Ekström <vojtechlevi1@hotmail.com>
2024-03-27 10:45:26 +0100

    loading effect when auth with github/figma

--- src/pages/Home.jsx
+++ src/pages/Home.jsx
@@ -1,33 +1,45 @@
 import React, { useContext } from "react";
 import { Link } from "react-router-dom";
 
+import UserContext from "../UserContext";
+import yrgologo from "../assets/yrgo-logo.png";
+
 const Home = () => {
+  const { user, setUser } = useContext(UserContext);
+
   return (
     <>
-      <main className="w-full h-screen flex flex-col justify-around items-center">
-        <div className="w-full px-8 flex flex-col gap-6 justify-center items-center">
-          <h1 className="text-8xl">Event 24 april</h1>
-          <p className="text-base">
-            Välkomna - mingel - Webbutvecklare och Digital Designers på YRGO -
-            det blir kul!
-          </p>
+      {user !== null ? (
+        <div className="flex h-screen w-full flex-col items-center justify-center gap-4">
+          <img src={yrgologo} alt="yrgo logo" className="" />
+          <p>Loading...</p>
         </div>
-        <div className="flex flex-col gap-4 justify-center items-center">
-          <p className="text-xl">Vem är du?</p>
-          <div className="flex gap-4">
-            <Link to="/student">
-              <button className="border border-black rounded-[32px] px-4 py-3">
-                Student
-              </button>
-            </Link>
-            <Link to="/company">
-              <button className="border border-black rounded-[32px] px-4 py-3">
-                Företag
-              </button>
-            </Link>
+      ) : (
+        <main className="flex h-screen w-full flex-col items-center justify-around">
+          <div className="flex w-full flex-col items-center justify-center gap-6 px-8">
+            <h1 className="text-8xl">Event 24 april</h1>
+            <p className="text-base">
+              Välkomna - mingel - Webbutvecklare och Digital Designers på YRGO -
+              det blir kul!
+            </p>
           </div>
-        </div>
-      </main>
+          <div className="flex flex-col items-center justify-center gap-4">
+            <p className="text-xl">Vem är du?</p>
+            <div className="flex gap-4">
+              <Link to="/student">
+                <button className="rounded-[32px] border border-black px-4 py-3">
+                  Student
+                </button>
+              </Link>
+              <Link to="/company">
+                <button className="rounded-[32px] border border-black px-4 py-3">
+                  Företag
+                </button>
+              </Link>
+            </div>
+          </div>
+        </main>
+      )}
     </>
   );
 };
--- src/pages/Login.jsx
+++ src/pages/Login.jsx
@@ -1,4 +1,4 @@
-import React, { useState, useEffect, useContext } from "react";
+import React, { useState, useContext } from "react";
 import { Link, useNavigate } from "react-router-dom";
 import { MoveLeft } from "lucide-react";
 
