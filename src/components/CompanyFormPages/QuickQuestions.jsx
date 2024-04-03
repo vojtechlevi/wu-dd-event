b381926b85d4c91c2e959066e2e3834833fa6988
JoarHansson <124060119+JoarHansson@users.noreply.github.com>
2024-04-03 15:47:19 +0200

    update styling and logic for QuickQuestions component

--- src/components/CompanyFormPages/QuickQuestions.jsx
+++ src/components/CompanyFormPages/QuickQuestions.jsx
@@ -1,115 +1,183 @@
 import { useState } from "react";
 
-const QuickQuestions = ({ counter, setCounter, answer, setAnswer }) => {
+const QuickQuestions = ({ answer, setAnswer }) => {
+  const ListItem = ({ item, onClick, defaultChecked }) => {
+    return (
+      <li
+        key={item}
+        className={"align-center flex  border-yrgo-red bg-white p-4"}
+      >
+        <input
+          className="my-auto h-4 w-4 shrink-0 appearance-none rounded-full border-2 border-yrgo-red bg-white checked:bg-yrgo-red"
+          type="checkbox"
+          value={item}
+          id={item}
+          onClick={onClick}
+          defaultChecked={defaultChecked}
+        />
+        <label
+          htmlFor={item}
+          className="my-auto pl-4 font-extrabold uppercase text-yrgo-red"
+        >
+          {item}
+        </label>
+        <br />
+      </li>
+    );
+  };
+
+  const updateAnswer = (question, value) => {
+    setAnswer({
+      ...answer,
+      quickQuestions: {
+        dogPolicy: question === "dogPolicy" ? value : dogPolicy,
+        remoteWorkPolicy:
+          question === "remoteWorkPolicy" ? value : remoteWorkPolicy,
+        officeLocation: question === "officeLocation" ? value : officeLocation,
+        companyType: question === "companyType" ? value : companyType,
+        officeType: question === "officeType" ? value : officeType,
+      },
+    });
+  };
+
+  // dog policy
   const [dogPolicy, setDogPolicy] = useState(
-    answer.quickQuestions.dogPolicy || ""
+    answer.quickQuestions.dogPolicy || "",
   );
-
   const updateDogPolicy = (event) => {
-    if (dogPolicy === event.target.textContent) {
-      setDogPolicy("");
-    } else {
-      setDogPolicy(event.target.textContent);
-    }
+    setDogPolicy(event.target.value);
+    updateAnswer("dogPolicy", event.target.value);
   };
 
+  // remote work policy
   const [remoteWorkPolicy, setRemoteWorkPolicy] = useState(
-    answer.quickQuestions.remoteWorkPolicy || ""
+    answer.quickQuestions.remoteWorkPolicy || "",
   );
-
   const updateRemoteWorkPolicy = (event) => {
-    if (remoteWorkPolicy === event.target.textContent) {
-      setRemoteWorkPolicy("");
-    } else {
-      setRemoteWorkPolicy(event.target.textContent);
-    }
+    setRemoteWorkPolicy(event.target.value);
+    updateAnswer("remoteWorkPolicy", event.target.value);
   };
 
+  // office location
   const [officeLocation, setOfficeLocation] = useState(
-    answer.quickQuestions.officeLocation || ""
+    answer.quickQuestions.officeLocation || "",
+  );
+  const updateOfficeLocation = (event) => {
+    setOfficeLocation(event.target.value);
+    updateAnswer("officeLocation", event.target.value);
+  };
+
+  // company type
+  const [companyType, setCompanyType] = useState(
+    answer.quickQuestions.companyType || "",
   );
 
-  const updateOfficeLocation = (event) => {
-    if (officeLocation === event.target.textContent) {
-      setOfficeLocation("");
-    } else {
-      setOfficeLocation(event.target.textContent);
-    }
+  const updateCompanyType = (event) => {
+    setCompanyType(event.target.value);
+    updateAnswer("companyType", event.target.value);
   };
 
-  // add more of the above if there are more "quick questions"
+  // office type
+  const [officeType, setOfficeType] = useState(
+    answer.quickQuestions.officeType || "",
+  );
+
+  const updateOfficeType = (event) => {
+    setOfficeType(event.target.value);
+    updateAnswer("officeType", event.target.value);
+  };
 
   return (
     <>
-      <h2 className="text-4xl">3 snabba</h2>
-      <div className="grid grid-cols-2 gap-4">
-        <button
-          className={`border border-black p-2 self-start ${
-            dogPolicy === "No dogs" ? "bg-black text-white" : ""
-          }`}
+      <h2 className="mt-16 border-b-4 border-yrgo-red p-4 text-2xl font-extrabold uppercase text-yrgo-red  ">
+        5 snabba!
+      </h2>
+      <ul className="grid grid-cols-2 gap-0.5 border-b-2 border-yrgo-red bg-yrgo-red">
+        <ListItem
+          item="hund = ja!"
           onClick={updateDogPolicy}
-        >
-          No dogs
-        </button>
-        <button
-          className={`border border-black p-2 self-start ${
-            dogPolicy === "Hundar = JA" ? "bg-black text-white" : ""
-          }`}
+          defaultChecked={
+            answer.quickQuestions.dogPolicy === "hund = ja!" ? true : false
+          }
+        />
+        <ListItem
+          item="no dogs :)"
           onClick={updateDogPolicy}
-        >
-          Hundar = JA
-        </button>
-        <button
-          className={`border border-black p-2 self-start ${
-            remoteWorkPolicy === "Remote work = NEJ"
-              ? "bg-black text-white"
-              : ""
-          }`}
+          defaultChecked={
+            answer.quickQuestions.dogPolicy === "no dogs :)" ? true : false
+          }
+        />
+
+        <ListItem
+          item="remote work = no"
           onClick={updateRemoteWorkPolicy}
-        >
-          Remote work = NEJ
-        </button>
-        <button
-          className={`border border-black p-2 self-start ${
-            remoteWorkPolicy === "Remote work = JA" ? "bg-black text-white" : ""
-          }`}
+          defaultChecked={
+            answer.quickQuestions.remoteWorkPolicy === "remote work = no"
+              ? true
+              : false
+          }
+        />
+        <ListItem
+          item="remote work = yes"
           onClick={updateRemoteWorkPolicy}
-        >
-          Remote work = JA
-        </button>
-        <button
-          className={`border border-black p-2 self-start ${
-            officeLocation === "Kontor i Sverige" ? "bg-black text-white" : ""
-          }`}
+          defaultChecked={
+            answer.quickQuestions.remoteWorkPolicy === "remote work = yes"
+              ? true
+              : false
+          }
+        />
+
+        <ListItem
+          item="kontor i sverige"
           onClick={updateOfficeLocation}
-        >
-          Kontor i Sverige
-        </button>
-        <button
-          className={`border border-black p-2 self-start ${
-            officeLocation === "Kontor utomlands" ? "bg-black text-white" : ""
-          }`}
+          defaultChecked={
+            answer.quickQuestions.officeLocation === "kontor i sverige"
+              ? true
+              : false
+          }
+        />
+        <ListItem
+          item="kontor utomlands"
           onClick={updateOfficeLocation}
-        >
-          Kontor utomlands
-        </button>
-      </div>
-      <button
-        onClick={() => {
-          setCounter(counter + 1);
-          setAnswer({
-            ...answer,
-            quickQuestions: {
-              dogPolicy: dogPolicy,
-              remoteWorkPolicy: remoteWorkPolicy,
-              officeLocation: officeLocation,
-            },
-          });
-        }}
-        className="border border-black p-2"
-      >
-        Nästa
-      </button>
+          defaultChecked={
+            answer.quickQuestions.officeLocation === "kontor utomlands"
+              ? true
+              : false
+          }
+        />
+
+        <ListItem
+          item="inhouse"
+          onClick={updateCompanyType}
+          defaultChecked={
+            answer.quickQuestions.companyType === "inhouse" ? true : false
+          }
+        />
+        <ListItem
+          item="byrå"
+          onClick={updateCompanyType}
+          defaultChecked={
+            answer.quickQuestions.companyType === "byrå" ? true : false
+          }
+        />
+
+        <ListItem
+          item="eget kontor"
+          onClick={updateOfficeType}
+          defaultChecked={
+            answer.quickQuestions.officeType === "eget kontor" ? true : false
+          }
+        />
+        <ListItem
+          item="kontors-landskap"
+          onClick={updateOfficeType}
+          defaultChecked={
+            answer.quickQuestions.officeType === "kontors-landskap"
+              ? true
+              : false
+          }
+        />
+      </ul>
     </>
   );
 };
