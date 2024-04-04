816ea2a8db8d056924a59f663168409d1d1b1dc3
JoarHansson <124060119+JoarHansson@users.noreply.github.com>
2024-04-04 16:14:53 +0200

    update Type component according to new design
    custom range slider styles are done except the handle

--- src/components/CompanyFormPages/Type.jsx
+++ src/components/CompanyFormPages/Type.jsx
@@ -1,44 +1,49 @@
 import { useState } from "react";
 
-const Type = ({ counter, setCounter, answer, setAnswer }) => {
-  const [type, setType] = useState(answer.type || "");
+// defaultFormValues:
+// companyType:
+//   {
+//     cool: null,
+//     fast: null,
+//     stable: null,
+//   },
+// ,
 
-  const updateType = (event) => {
-    if (type === event.target.textContent) {
-      setType("");
-    } else {
-      setType(event.target.textContent);
-    }
-  };
+const ListItem = ({ sliderName, sliderText }) => {
+  return (
+    <li key={sliderName} className="m-4 mt-8">
+      <p className="mb-8 font-extrabold uppercase text-yrgo-red">
+        {sliderText}
+      </p>
+      <input
+        type="range"
+        min="1"
+        max="10"
+        defaultValue="5"
+        class="w-full"
+        id="myRange"
+      />
+      <div className="flex justify-between">
+        <p className="font-bold text-yrgo-red">1</p>
+        <p className="font-bold text-yrgo-red">10</p>
+      </div>
+    </li>
+  );
+};
+
+const Type = ({ answer, setAnswer }) => {
+  const [type, setType] = useState(answer.type || "");
 
   return (
     <>
-      <h2 className="text-4xl">Vilken sorts arbetsplats är ni?</h2>
-      <button
-        className={`border border-black p-2 self-start ${
-          type === "Byrå" ? "bg-black text-white" : ""
-        }`}
-        onClick={updateType}
-      >
-        Byrå
-      </button>
-      <button
-        className={`border border-black p-2 self-start ${
-          type === "Inhouse" ? "bg-black text-white" : ""
-        }`}
-        onClick={updateType}
-      >
-        Inhouse
-      </button>
-      <button
-        onClick={() => {
-          setCounter(counter + 1);
-          setAnswer({ ...answer, type: type });
-        }}
-        className="border border-black p-2"
-      >
-        Nästa
-      </button>
+      <h2 className="mt-16 border-b-4 border-yrgo-red p-4 text-2xl font-extrabold uppercase text-yrgo-red  ">
+        Vilken rörelse är ditt företag?
+      </h2>
+      <ul>
+        <ListItem sliderName={"cool"} sliderText={"cool som en katt"} />
+        <ListItem sliderName={"fast"} sliderText={"snabb som blixten"} />
+        <ListItem sliderName={"stable"} sliderText={"stadig och stabil"} />
+      </ul>
     </>
   );
 };
