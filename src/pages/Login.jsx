5b01111137aadaae414f002bdc2b264e97bdd0e9
Levi Ekström <vojtechlevi1@hotmail.com>
2024-04-05 13:15:35 +0200

    added styling to home, login, signup, student also onclick on company in student list

--- src/pages/Login.jsx
+++ src/pages/Login.jsx
@@ -1,11 +1,9 @@
 import React, { useState, useContext } from "react";
 import { Link, useNavigate } from "react-router-dom";
-import { MoveLeft } from "lucide-react";
 
 import { supabase } from "../client";
 import UserContext from "../UserContext";
-import GithubIcon from "../assets/github-icon.svg";
-import FigmaIcon from "../assets/figma-icon.svg";
+import ThemeBox from "../components/ThemeBox";
 
 const Login = () => {
   let navigate = useNavigate();
@@ -15,18 +13,6 @@ const Login = () => {
     email: "",
     password: "",
   });
-
-  async function signInWithFigma() {
-    const { data, error } = await supabase.auth.signInWithOAuth({
-      provider: "figma",
-    });
-  }
-
-  async function signInWithGithub() {
-    const { data, error } = await supabase.auth.signInWithOAuth({
-      provider: "github",
-    });
-  }
 
   function handleChange(event) {
     setErrorMessage("");
@@ -77,69 +63,48 @@ const Login = () => {
   }
   return (
     <>
-      <section className="w-full h-screen flex flex-col justify-center items-center">
-        <div className="px-4 w-[320px]">
-          <div className="w-full p mb-8 text-[#d2d2d2]">
-            <Link to="/">
-              <MoveLeft size={32} />
+      <ThemeBox backBtn="/">
+        <div className="mb-8 pl-2">
+          <h2 className=" text-5xl font-extrabold uppercase">Logga in</h2>
+          <p>Kort text om varför skapa konto: spara favoriter,</p>
+        </div>
+        <form onSubmit={signInWithEmail} className="flex w-full flex-col">
+          <input
+            onChange={handleChange}
+            type="email"
+            name="email"
+            placeholder="Mailadress"
+            className="w-full border-t-2 border-yrgo-blue px-4 py-6 outline-none"
+          />
+          <input
+            onChange={handleChange}
+            type="password"
+            name="password"
+            placeholder="Lösenord"
+            className="w-full border-t-2 border-yrgo-blue px-4 py-6 outline-none"
+          />
+          {errorMessage ? (
+            <div className="w-full border-t-2 border-yrgo-blue ">
+              <p className="py-4 text-center text-yrgo-blue">{errorMessage}</p>
+            </div>
+          ) : (
+            ""
+          )}
+          <div className="flex h-12 w-full border-y-2 border-yrgo-blue">
+            <button
+              className="h-full w-1/2 bg-yrgo-blue px-2 font-extrabold uppercase text-white"
+              type="submit"
+            >
+              Logga In
+            </button>
+            <Link to="/signup" className="h-full w-1/2 ">
+              <button className=" h-full w-full px-2 font-extrabold uppercase text-yrgo-blue">
+                Skapa Konto
+              </button>
             </Link>
           </div>
-          <div className="w-full flex flex-col gap-6 justify-center px-8">
-            <h1 className="text-6xl">Logga in</h1>
-            <p className="text-base w-full justify-center">
-              Kort text om varför skapa konto: spara favoriter,
-            </p>
-          </div>
-          <form
-            onSubmit={signInWithEmail}
-            className="flex flex-col gap-6 my-20 w-full items-center"
-          >
-            <input
-              type="email"
-              name="email"
-              placeholder="Mailadress"
-              onChange={handleChange}
-              className="bg-[#DEDEDE] rounded-lg p-2 w-[250px]"
-            />
-            <input
-              type="password"
-              name="password"
-              placeholder="Lösenord"
-              onChange={handleChange}
-              className="bg-[#DEDEDE] rounded-lg p-2 w-[250px]"
-            />
-            <p className="text-xs text-center text-red-600">{errorMessage}</p>
-            <div className="flex gap-4">
-              <h2>eller logga in med:</h2>
-              <img
-                onClick={signInWithGithub}
-                src={GithubIcon}
-                alt="Github Sign in"
-                className="w-5 cursor-pointer"
-              />
-              <img
-                onClick={signInWithFigma}
-                src={FigmaIcon}
-                alt="Github Sign in"
-                className="w-5 cursor-pointer"
-              />
-            </div>
-            <div className="flex gap-4">
-              <button
-                type="submit"
-                className="border border-black bg-black text-white text-xl rounded-[32px] px-4 py-3"
-              >
-                Logga In
-              </button>
-              <Link to="/signup">
-                <button className="border border-black text-xl rounded-[32px] px-4 py-3">
-                  Skapa Konto
-                </button>
-              </Link>
-            </div>
-          </form>
-        </div>
-      </section>
+        </form>
+      </ThemeBox>
     </>
   );
 };
--- src/pages/Test.jsx
+++ src/pages/Test.jsx
@@ -1,12 +1,160 @@
-import React from "react";
-import { Link } from "react-router-dom";
+import React, { useState, useEffect } from "react";
+import { Link, useNavigate } from "react-router-dom";
+import { MoveLeft } from "lucide-react";
+
+import { supabase } from "../client";
 
 import ThemeBox from "../components/ThemeBox";
 
 const Test = () => {
+  const navigate = useNavigate();
+  const [isSubmitted, setIsSubmitted] = useState(true);
+  const [countdown, setCountdown] = useState(null);
+  const [errorMessage, setErrorMessage] = useState("");
+  const [formData, setFormData] = useState({
+    fullName: "",
+    email: "",
+    password: "",
+  });
+
+  function handleChange(event) {
+    setErrorMessage("");
+    setFormData((prevFormData) => {
+      return {
+        ...prevFormData,
+        [event.target.name]: event.target.value,
+      };
+    });
+  }
+
+  function translateErrorMessage(message) {
+    const errorTranslations = {
+      "User already registered":
+        "En användare med denna e-postadress finns redan",
+      "Signup requires a valid password":
+        "Registrering kräver ett giltigt lösenord",
+      "Password should be at least 6 characters.":
+        "Lösenordet måste vara minst 6 tecken långt",
+    };
+
+    return errorTranslations[message] || message;
+  }
+
+  async function handleSubmit(e) {
+    e.preventDefault();
+
+    if (formData.fullName.trim() === "") {
+      setErrorMessage("Du behöver ange ditt namn");
+      return;
+    }
+    try {
+      const { data, error } = await supabase.auth.signUp({
+        email: formData.email,
+        password: formData.password,
+        options: {
+          data: {
+            full_name: formData.fullName,
+          },
+        },
+      });
+      if (error) {
+        setErrorMessage(translateErrorMessage(error.message));
+      } else if (data) {
+        setIsSubmitted(true);
+        setCountdown(5);
+        // Sign the user out after sign-up
+        await supabase.auth.signOut();
+      }
+    } catch (error) {
+      alert(error);
+    }
+  }
+
+  useEffect(() => {
+    if (countdown > 0) {
+      const timerId = setTimeout(() => {
+        setCountdown(countdown - 1);
+      }, 1000);
+      return () => clearTimeout(timerId);
+    } else if (countdown === 0) {
+      navigate("/login");
+    }
+  }, [countdown, navigate]);
   return (
     <>
-      <ThemeBox></ThemeBox>
+      <ThemeBox backBtn="/login">
+        {isSubmitted ? (
+          <div className="flex flex-col gap-6">
+            <div className="flex w-full flex-col gap-6 px-16">
+              <p className="w-full text-xl">
+                Ditt konto är skapat! <br />
+              </p>
+              <h1 className="text-5xl font-extrabold uppercase">
+                {formData.fullName}
+              </h1>
+              <p>
+                Logga in på din profil och sök bland företag och kanske hitta en
+                LIA plats?
+              </p>
+              <p className="text-xs">Du omdirigeras om: {countdown}</p>
+            </div>
+            <Link to="/login">
+              <button className=" border-yrgo-blue bg-yrgo-blue w-full border-y-2 px-4 py-3 font-extrabold uppercase text-white">
+                Logga In
+              </button>
+            </Link>
+          </div>
+        ) : (
+          <div className="">
+            <div className="mb-8 text-center">
+              <h2 className=" text-5xl font-extrabold uppercase">
+                Skapa Konto
+              </h2>
+              <p>Kort text om varför skapa konto: spara favoriter,</p>
+            </div>
+            <form onSubmit={handleSubmit} className="flex w-full flex-col ">
+              <input
+                type="text"
+                name="fullName"
+                placeholder="Namn"
+                onChange={handleChange}
+                className="border-yrgo-blue w-full border-t-2 px-4 py-6 focus:outline-none"
+              />
+              <input
+                type="email"
+                name="email"
+                placeholder="Mailadress"
+                onChange={handleChange}
+                className="border-yrgo-blue w-full border-t-2 px-4 py-6 focus:outline-none"
+              />
+              <input
+                type="password"
+                name="password"
+                placeholder="Lösenord"
+                onChange={handleChange}
+                className="border-yrgo-blue w-full border-t-2 px-4 py-6 focus:outline-none"
+              />
+              {errorMessage ? (
+                <div className="border-yrgo-blue w-full border-t-2 ">
+                  <p className="text-yrgo-blue py-4 text-center">
+                    {errorMessage}
+                  </p>
+                </div>
+              ) : (
+                ""
+              )}
+              <div className="flex justify-start">
+                <button
+                  type="submit"
+                  className="border-yrgo-blue bg-yrgo-blue flex h-12 w-full items-center justify-center border-y-2 font-extrabold uppercase text-white"
+                >
+                  Skapa Konto
+                </button>
+              </div>
+            </form>
+          </div>
+        )}
+      </ThemeBox>
     </>
   );
 };
