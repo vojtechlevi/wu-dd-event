10e02989a73719555396d6804ddb0f193db10979
JoarHansson <124060119+JoarHansson@users.noreply.github.com>
2024-04-09 09:37:24 +0200

    add choice for Link or button

--- src/components/Buttons/ButtonArrowRight.jsx
+++ src/components/Buttons/ButtonArrowRight.jsx
@@ -1,13 +1,35 @@
+import { Link } from "react-router-dom";
 import { MoveRight } from "lucide-react";
 
-export default function ButtonArrowRight({ children, onClick }) {
+export default function ButtonArrowRight({
+  children,
+  onClick,
+  isLink = false,
+  linkTo,
+}) {
   return (
-    <button
-      onClick={onClick}
-      className=" flex w-1/2 items-center justify-end border-2 border-yrgo-red bg-white p-4"
-    >
-      <p className="mr-4 font-extrabold uppercase  text-yrgo-red">{children}</p>
-      <MoveRight className=" stroke-yrgo-red" />
-    </button>
+    <>
+      {isLink === true ? (
+        <Link
+          to={linkTo}
+          className=" flex w-1/2 items-center justify-end border-2 border-yrgo-red bg-white p-4"
+        >
+          <p className="mr-4 font-extrabold uppercase  text-yrgo-red">
+            {children}
+          </p>
+          <MoveRight className=" stroke-yrgo-red" />
+        </Link>
+      ) : (
+        <button
+          onClick={onClick}
+          className=" flex w-1/2 items-center justify-end border-2 border-yrgo-red bg-white p-4"
+        >
+          <p className="mr-4 font-extrabold uppercase  text-yrgo-red">
+            {children}
+          </p>
+          <MoveRight className=" stroke-yrgo-red" />
+        </button>
+      )}
+    </>
   );
 }
