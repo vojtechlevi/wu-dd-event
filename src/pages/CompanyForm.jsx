5bb40db2f465b630b671bcf573aed482788a4023
JoarHansson <124060119+JoarHansson@users.noreply.github.com>
2024-04-09 09:42:05 +0200

    Make some improvements to CompanyForm component
    * remove unused imports
    * fix counter progress bar
    * Add routes from first and last page
    * change to more logic html elements

--- src/pages/CompanyForm.jsx
+++ src/pages/CompanyForm.jsx
@@ -3,9 +3,6 @@ import { useState } from "react";
 import ButtonArrowRight from "../components/Buttons/ButtonArrowRight";
 import ButtonArrowLeft from "../components/Buttons/ButtonArrowLeft";
 
-// todo: make Greeting a separate page instead of a form page
-// import Greeting from "../components/CompanyFormPages/Greeting";
-
 import Contact from "../components/CompanyFormPages/Contact";
 import Employees from "../components/CompanyFormPages/Employees";
 import FocusAreas from "../components/CompanyFormPages/FocusAreas";
@@ -16,12 +13,6 @@ import Type from "../components/CompanyFormPages/Type";
 import InternTypeCount from "../components/CompanyFormPages/InternTypeCount";
 import InternshipDuration from "../components/CompanyFormPages/InternshipDuration";
 import PreviewPost from "../components/CompanyFormPages/PreviewPost";
-
-// todo: make Confirmation a popup from preview post instead
-import Confirmation from "../components/CompanyFormPages/Confirmation";
-
-// todo: make EventStats a separate page  instead of a form page
-import EventStats from "../components/CompanyFormPages/EventStats";
 
 const CompanyForm = () => {
   const [counter, setCounter] = useState(0);
@@ -57,7 +48,6 @@ const CompanyForm = () => {
   });
 
   const formPages = [
-    // Greeting,
     Contact,
     Employees,
     FocusAreas,
@@ -68,32 +58,13 @@ const CompanyForm = () => {
     InternTypeCount,
     InternshipDuration,
     PreviewPost,
-    Confirmation,
-    EventStats,
   ];
 
   const FormPage = formPages[counter];
 
-  const formQuestions = [
-    // these are counted in the progress bar (last 3 from above excluded)
-    // Greeting,
-    Contact,
-    Employees,
-    FocusAreas,
-    SoftwaresDesign,
-    SoftwaresDev,
-    QuickQuestions,
-    Type,
-    InternTypeCount,
-    InternshipDuration,
-  ];
-
-  // Instead of "Fråga X/Y", for the last pages that aren't questions:
-  const message = ["Preview", "Confirmation", "Stats"];
-
   return (
-    <section className="relative grid h-full w-full grid-rows-layout">
-      <div className=" w-full bg-white">
+    <main className="relative grid h-full w-full grid-rows-layout">
+      <section className=" w-full bg-white">
         <div className=" h-12 w-full bg-yrgo-red"></div>
         <div className="mx-4 border-x-2  border-yrgo-red">
           <div className="flex justify-between  border-b-2 border-yrgo-red px-4">
@@ -102,48 +73,57 @@ const CompanyForm = () => {
             </p>
             <div className="border border-yrgo-red"></div>
             <p className=" my-1 font-extrabold uppercase text-yrgo-red">
-              {counter < formQuestions.length
-                ? `fråga ${counter}/${formQuestions.length - 1}`
-                : message[counter - formQuestions.length]}
+              fråga {counter + 1}/{formPages.length - 1}
             </p>
           </div>
           <div className=" flex h-6 w-full justify-between border-b-2 border-yrgo-red">
-            {formQuestions.map((x) => {
+            {formPages.map((page) => {
               return (
                 <div
-                  className={`${formQuestions.indexOf(x) < counter ? "bg-yrgo-red" : ""} w-full`}
-                  key={formQuestions.indexOf(x)}
+                  className={`${formPages.indexOf(page) <= counter ? "bg-yrgo-red" : ""} w-full`}
+                  key={formPages.indexOf(page)}
                 ></div>
               );
             })}
           </div>
         </div>
-      </div>
+      </section>
 
-      <div className=" mx-4 overflow-y-auto border-x-2 border-yrgo-red pt-8">
+      <section className=" mx-4 overflow-y-auto border-x-2 border-yrgo-red pt-8">
         <FormPage
           counter={counter}
           setCounter={setCounter}
           answer={answer}
           setAnswer={setAnswer}
         />
-      </div>
+      </section>
 
-      <div className="mb-4 flex w-full px-4">
-        <ButtonArrowLeft
-          onClick={() => setCounter(counter > 0 ? counter - 1 : counter)}
-        >
-          tillbaka
-        </ButtonArrowLeft>
-        <ButtonArrowRight
-          onClick={() => {
-            setCounter(counter + 1);
-          }}
-        >
-          nästa
-        </ButtonArrowRight>
-      </div>
-    </section>
+      <section className="mb-4 flex w-full px-4">
+        {counter === 0 ? (
+          <ButtonArrowLeft isLink={true} linkTo={"/company"}>
+            tillbaka
+          </ButtonArrowLeft>
+        ) : (
+          <ButtonArrowLeft onClick={() => setCounter(counter - 1)}>
+            tillbaka
+          </ButtonArrowLeft>
+        )}
+
+        {counter === formPages.length - 1 ? (
+          <ButtonArrowRight isLink={true} linkTo={"/event-info"}>
+            klar
+          </ButtonArrowRight>
+        ) : (
+          <ButtonArrowRight
+            onClick={() => {
+              setCounter(counter + 1);
+            }}
+          >
+            nästa
+          </ButtonArrowRight>
+        )}
+      </section>
+    </main>
   );
 };
 
