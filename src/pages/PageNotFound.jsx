4fa4ad5ac3350a13b16fe5ba7b78473bd0e7f692
Levi Vojtech Stefan Ekström <91605543+vojtechlevi@users.noreply.github.com>
2024-04-16 12:00:10 +0200

    Update PageNotFound.jsx

--- src/pages/PageNotFound.jsx
+++ src/pages/PageNotFound.jsx
@@ -5,7 +5,7 @@ import { Link } from "react-router-dom";
 const PageNotFound = () => {
   return (
     <>
-      <div className="hidden h-full w-full items-center justify-center lg:flex">
+      <div className=" flex h-full w-full items-center justify-center">
         <div className="flex h-full w-full flex-col items-center justify-center bg-yrgo-red text-white lg:h-auto lg:w-[475px] lg:py-12">
           <h2 className="text-[96px] font-extrabold uppercase">OOPS!</h2>
           <p className="-mt-6 text-2xl font-extrabold uppercase">
